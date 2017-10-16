3ded2174f215d002e5608ed3e044242bc601a180
Yuri Dirickson <ydirickson@gmail.com>
2017-10-15 23:40:00 -0200

    Melhorando script de deploy

--- deploy.cmd
+++ deploy.cmd
@@ -139,8 +139,10 @@ IF EXIST "%DEPLOYMENT_TARGET%\manage.py" (
   IF EXIST "%DEPLOYMENT_TARGET%\env\lib\site-packages\django" (
     IF NOT EXIST "%DEPLOYMENT_TARGET%\.skipDjango" (
       echo First lets run Django tests
+      
       env\scripts\python manage.py test
-
+      IF %ERRORLEVEL% GEQ 1 EXIT /B 2
+      
       echo Collecting Django static files. You can skip Django specific steps with a .skipDjango file.
       IF NOT EXIST "%DEPLOYMENT_TARGET%\static" (
         MKDIR "%DEPLOYMENT_TARGET%\static"
